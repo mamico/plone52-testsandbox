5ccf6608ca0ca5b65a8d0b96a4cec6b0313e1cb1
mauro <mauro.amico@unibo.it>
2019-08-14 17:27:19 +0200

    threads

--- scripts/blob.py
+++ scripts/blob.py
@@ -1,12 +1,20 @@
 from plone import api
 import os
 from plone.namedfile.file import NamedBlobFile
+import sys
+import transaction
 
 
 portal = api.portal.get()
 
+SIZE = 1024 * 1024 * 1024
 if 'largefile' not in portal:
     obj = api.content.create(portal, id="largefile", type="File")
-    SIZE = 256 * 1024 * 1024
     obj.file = NamedBlobFile(data=os.urandom(SIZE), filename=u'dummy.bin')
+elif '--overrides' in sys.argv:
+    obj = portal.largefile
+    obj.file = NamedBlobFile(data=os.urandom(SIZE), filename=u'dummy.bin')
+
+if '--commit' in sys.argv:
+    transaction.commit()
 
