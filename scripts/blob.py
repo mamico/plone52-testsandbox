279d9ddaf6b9531302f66b6bae835a678c2cdd8a
mauro <mauro.amico@unibo.it>
2019-08-14 15:58:44 +0200

    bjoern

---
+++ scripts/blob.py
@@ -0,0 +1,12 @@
+from plone import api
+import os
+from plone.namedfile.file import NamedBlobFile
+
+
+portal = api.portal.get()
+
+if 'largefile' not in portal:
+    obj = api.content.create(portal, id="largefile", type="File")
+    SIZE = 256 * 1024 * 1024
+    obj.file = NamedBlobFile(data=os.urandom(SIZE), filename='dummy.bin')
+
