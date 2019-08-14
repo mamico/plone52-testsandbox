46dd0c357ce902cd26a9263a265c60a07136290e
mauro <mauro.amico@unibo.it>
2019-08-14 16:00:43 +0200

    zserver python 2.7

--- scripts/blob.py
+++ scripts/blob.py
@@ -8,5 +8,5 @@ portal = api.portal.get()
 if 'largefile' not in portal:
     obj = api.content.create(portal, id="largefile", type="File")
     SIZE = 256 * 1024 * 1024
-    obj.file = NamedBlobFile(data=os.urandom(SIZE), filename='dummy.bin')
+    obj.file = NamedBlobFile(data=os.urandom(SIZE), filename=u'dummy.bin')
 
